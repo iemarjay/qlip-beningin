0f15c72a54a4c53dbda376a84cb3f22f56f663c2
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2021-12-21 12:02:23 +0100

    Update Header.vue

--- components/Header.vue
+++ components/Header.vue
@@ -1,30 +1,33 @@
 <template>
   <div class="max-w-6xl px-4 py-4 mx-auto">
-    <div class="flex justify-center mb-4">
-      <nuxt-link to="/">
-        <logo class="h-7 md:h-9" />
-      </nuxt-link>
+    <div class="grid grid-cols-5 mb-4">
+      <div></div>
+      <div class="text-center col-span-3 flex justify-center">
+        <nuxt-link to="/">
+          <logo class="h-7 md:h-9" />
+        </nuxt-link>
+      </div>
+      <div class="text-right">
+        <button class="md:hidden" @click="showMobileNav = !showMobileNav">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-6 w-6"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </button>
+      </div>
     </div>
     <div class="text-white flex items-center font-sans">
       <div class="w-full flex items-center justify-center mx-auto flex-wrap">
-        <div class="flex ml-auto md:ml-6 md:order-3">
-          <button class="md:hidden" @click="showMobileNav = !showMobileNav">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
-        </div>
 
         <div
           class="md:block text-base lg:text-lg font-medium w-full md:w-auto flex flex-col md:flex-row"
