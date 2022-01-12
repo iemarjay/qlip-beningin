0048e60051d92948d23fae8ae8a8b3c6c62f9b12
joseph emmanuel kayode (iemarjay) <ejoseph@wikimedia.org>
2022-01-12 21:06:40 +0100

    Icons

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,11 +5,28 @@ export default {
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
-      { hid: "description", name: "description", content: "" },
+      { hid: "description", name: "description", content: "In the “Beningin” there were 50 founding tribes of Africa, 28 were launched first to start the 1st generation,and those 29 each multiplied by 14 to become 490 strong and courageous GINS." },
       { name: "format-detection", content: "telephone=no" },
+      { hid: "theme-color", name: "theme-color", content: "#2A2A3C" },
+      { hid: "author", name: "author", content: "Qlip" },
+      { property: "og:type", name: "og:type", content: "website" },
+      {
+        hid: "msapplication-navbutton-color",
+        name: "msapplication-navbutton-color",
+        content: "#2A2A3C",
+      },
+      {
+        hid: "apple-mobile-web-app-status-bar-style",
+        name: "apple-mobile-web-app-status-bar-style",
+        content: "#2A2A3C",
+      },
+      { property: "og:site_name", name: "og:site_name", content: "Qlip" },
     ],
-    link: [{ rel: "icon", type: "image/png", href: "/icon.png" }],
-  },
+    link: [
+      { rel: "icon", type: "image/png", href: "/icon.png" },
+      { rel: "shortcut icon", href: "/icon.png" },
+    ],
+},
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
@@ -38,6 +55,13 @@ export default {
   pwa: {
     manifest: {
       lang: "en",
+      name: "Qlip Beningin - Africa’s No.1 cross-chain NFT platform",
+      short_name: "Qlip Beningin",
+      description:
+        "In the “Beningin” there were 50 founding tribes of Africa, 28 were launched first to start the 1st generation,and those 29 each multiplied by 14 to become 490 strong and courageous GINS.",
+    },
+    icon: {
+      source: "/icon.jpeg",
     },
   },
 
